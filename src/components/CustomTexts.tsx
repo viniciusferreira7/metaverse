86e748becbac5f96a9ff539773ab5e34e0d89435
Vinicius Ferreira <viniciuferreira32@gmail.com>
2023-03-29 17:23:20 -0300

    feat: make the TypingText component dynamic for styling

--- src/components/CustomTexts.tsx
+++ src/components/CustomTexts.tsx
@@ -1,7 +1,26 @@
 'use client'
 
-export function TypingText() {
-  return <p>Typing Text</p>
+import { motion } from 'framer-motion'
+import { textContainer, textVariant2 } from '@/utils/motion'
+
+interface TypingTextProps {
+  title: string
+  textStyles: string
+}
+
+export function TypingText({ title, textStyles }: TypingTextProps) {
+  return (
+    <motion.p
+      variants={textContainer}
+      className={`${textStyles} font-normal text-[17px] text-secondary-white `}
+    >
+      {Array.from(title).map((letter, index) => (
+        <motion.span key={index} variants={textVariant2}>
+          {letter === ' ' ? '\u00A0' : letter}
+        </motion.span>
+      ))}
+    </motion.p>
+  )
 }
 
 export function TitleText() {
