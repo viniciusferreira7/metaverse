a480bc441640f607028796aae7e95ba89c17529c
Vinicius Ferreira <viniciuferreira32@gmail.com>
2023-03-29 17:22:05 -0300

    style: inserted gradient backgrounds in the sessions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,12 +15,21 @@ export default function Home() {
     <div className="bg-primary-black overflow-hidden">
       <Navbar />
       <Hero />
+      <div className="relative">
       <About />
+        <div className="gradient-03 z-0" />
       <Explore />
+      </div>
+      <div className="relative">
       <GetStarted />
+        <div className="gradient-04 z-0" />
       <WhatsNew />
+      </div>
+      <div className="relative">
       <World />
+        <div className="gradient-04 z-0" />
       <Insights />
+      </div>
       <Feedback />
       <Footer />
     </div>
