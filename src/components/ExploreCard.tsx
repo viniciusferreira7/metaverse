7030e0042a4e339ca15c2a0488acc592e40ff247
Vinicius Ferreira <viniciuferreira32@gmail.com>
2023-03-30 15:38:22 -0300

    feat: makes cards and the animation of the exploreCard

--- src/components/ExploreCard.tsx
+++ src/components/ExploreCard.tsx
@@ -1,7 +1,82 @@
 'use client'
 
-function ExploreCard() {
-  return <div>Explore Card</div>
+import { motion } from 'framer-motion'
+import { fadeIn } from '@/utils/motion'
+import styles from '@/styles'
+import Image from 'next/image'
+
+interface ExploreCardProps {
+  active: string
+  index: number
+  handleClick: (id: string) => void
+  id: string
+  imgUrl: string
+  title: string
+}
+
+function ExploreCard({
+  active,
+  index,
+  handleClick,
+  id,
+  imgUrl,
+  title,
+}: ExploreCardProps) {
+  const isActiveId =
+    active === id ? 'lg:flex-[3.5] flex-[10]' : 'lg:flex-[0.5] flex-[2]'
+
+  return (
+    <motion.div
+      variants={fadeIn('right', 'spring', index * 0.5, 0.75)}
+      className={`relative ${isActiveId} flex 
+        items-center justify-center min-w-[170px] 
+        h-[700px] transition-[flex] duration-700 
+        ease-out-flex cursor-pointer`}
+      onClick={() => handleClick(id)}
+    >
+      <Image
+        src={imgUrl}
+        alt={title}
+        width={3000}
+        height={1000}
+        className="absolute w-full h-full rounded-[24px] object-cover"
+      />
+      {active !== id ? (
+        <h3
+          className="font-semibold sm:text-[26px] 
+            text-[18px] absolute z-0 
+            lg:bottom-32 lg:rotate-[-90deg] 
+            lg:origin-[0,0] text-white"
+        >
+          {title}
+        </h3>
+      ) : (
+        <div
+          className="absolute bottom-0 p-8 justify-start 
+            w-full flex-col 
+            bg-[rgba(0,0,0,0.5)] rounded-b-[24px]"
+        >
+          <div
+            className={`${styles.flexCenter} w-[60px] h-[60px] rounded-[24px] glassmorphism mb-[16px]`}
+          >
+            <Image
+              src="/headset.svg"
+              alt="Headset"
+              width={500}
+              height={500}
+              className="w-1/2 h-1/2 object-contain"
+            />
+          </div>
+          <p className="font-normal text-[16px] leading-[20px] text-white uppercase">
+            Enter the Metaverse
+          </p>
+          <h2 className="mt-[24px] font-semibold sm:text-[32px] text-[24px] text-white">
+            {title}
+          </h2>
+        </div>
+      )}
+    </motion.div>
+  )
 }
 
 export default ExploreCard
