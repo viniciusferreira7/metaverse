65e041550275770de7777e96e68f634f29113400
Vinicius Ferreira <viniciuferreira32@gmail.com>
2023-04-01 22:48:26 -0300

    style: applying styles to the starSteps component

--- src/components/StartSteps.tsx
+++ src/components/StartSteps.tsx
@@ -1,5 +1,23 @@
-function StartSteps() {
-  return <div>start steps</div>
+import styles from '../styles'
+
+interface StartStepsProps {
+  number: number
+  text: string
+}
+
+function StartSteps({ number, text }: StartStepsProps) {
+  return (
+    <div className={`${styles.flexCenter} flex-row`}>
+      <div
+        className={`${styles.flexCenter} w-[70px] h-[70px] rounded-[24px] bg-[#323f5d] `}
+      >
+        <p className="text-white font-bold text-[20px]">0{number}</p>
+      </div>
+      <p className="flex-1 ml-[30px] font-normal text-[18px] text-[#B0B0B0] leading-[32px]">
+        {text}
+      </p>
+    </div>
+  )
 }
 
 export default StartSteps
