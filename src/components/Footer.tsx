7921e23eed00b8b95c795bac4e37abee1de48410
Vinicius Ferreira <viniciuferreira32@gmail.com>
2023-04-06 12:05:28 -0300

    feat: finish footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,65 @@
 'use client'
 
+import { motion } from 'framer-motion'
+import { footerVariants } from '@/utils/motion'
+import { socials } from '../constants'
+import styles from '../styles'
+import Image from 'next/image'
+
 function Footer() {
-  return <footer>footer</footer>
+  return (
+    <motion.footer
+      variants={footerVariants}
+      initial="hidden"
+      whileInView="show"
+      className={`${styles.paddings} py-8 relative`}
+    >
+      <div className="footer-gradient" />
+      <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
+        <div className="flex items-center justify-between flex-wrap gap-5">
+          <h4 className="font-bold md:text-[64px] text-[44px] text-white">
+            Enter the Metaverse
+          </h4>
+          <button
+            type="button"
+            className="flex items-center h-fit py-4 px-6 bg-[#25618b] rounded-[32px] gap-[12px]"
+          >
+            <Image
+              src="/headset.svg"
+              alt="headset"
+              width={24}
+              height={24}
+              className="w-[24px] h-[24px] object-contain"
+            />
+            <span className="font-normal text-[16px] text-white uppercase">
+              Enter Metaverse
+            </span>
+          </button>
+        </div>
+        <div className="flex flex-col">
+          <div className="mb-[50px] h-[2px] bg-white opacity-10" />
+          <div className="flex flex-items justify-between flex-wrap gap-4">
+            <h1 className="font-extrabold text-[24px] text-white">Metaverse</h1>
+            <p className="font-normal text-[14px] text-white opacity-50">
+              Copyright © 2021 - 2023 Metaverse. All rights reserved.
+            </p>
+            <div className="flex gap-4">
+              {socials.map((social) => (
+                <Image
+                  key={social.name}
+                  src={social.url}
+                  alt={social.name}
+                  width={30}
+                  height={30}
+                  className="w-[24px] h-[24px] object-contain cursor-pointer"
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </motion.footer>
+  )
 }
 
 export default Footer
